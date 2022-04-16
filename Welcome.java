37435c40ee6900b294512b780955c6f016e77706
Sri-1227 <srinivasmekala1227@gmail.com>
2022-04-16 11:42:03 +0530

    new changes

--- Welcome.java
+++ Welcome.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Welcome {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("DON'T FORGET TO RUN CONTAINER IN INTERACTIVE ENVIRONMENT TO GET USER INPUTt");
-        System.out.println("Enter your name: ");
-        String name = in.next();
+        // Scanner in = new Scanner(System.in);
+
+        // String name = in.next();
+        String name = args[0];
         System.out.println("Hello " + name + "! Welcome to my first docker container");
 
     }
